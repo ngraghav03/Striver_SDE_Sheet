0281995af91d191d176d7e5f479d39644f307409
ngraghav03 <raghav.ng2021@vitstudent.ac.in>
2024-04-24 21:04:53 +0530

    Updated Detect Cycle in LL Sum

--- LinkedLists2/DetectCycleInLL/bruteforce.py
+++ LinkedLists2/DetectCycleInLL/bruteforce.py
@@ -36,3 +36,14 @@ if detectCycle(head):
     print("Loop detected in the linked list")
 else:
     print("No Loop detected in the linked list")
+
+# ? Time Complexity: O(N * 2 * log(N) )The algorithm traverses the linked list once, 
+# ?                  performing hashmap insertions and searches in the while loop for each node. 
+# ?                  The insertion and search operations in the unordered_map have a worst-case time complexity of O(log(N)). 
+# ?                  As the loop iterates through N nodes, the total time complexity is determined by 
+# ?                  the product of the traversal (O(N)) and the average-case complexity of the hashmap operations (insert and search), 
+# ?                  resulting in O(N * 2 * log(N)). 
+
+# ? Space Complexity: O(N), The code uses a hashmap/dictionary to store encountered nodes, which can take up to O(N) additional space, 
+# ?                   where 'n' is the number of nodes in the list. 
+# ?                   Hence, the spacecomplexity is O(N) due to the use of the map to track nodes.
