609c70450436377a2e5417390054ddb775d0c505
ngraghav03 <raghav.ng2021@vitstudent.ac.in>
2024-07-24 19:12:06 +0530

    Updated Subset Sum Problem

--- Recursion/SubsetSum/solution.py
+++ Recursion/SubsetSum/solution.py
@@ -15,7 +15,7 @@ class Solution:
 
     def findAllSubsetSums(self, arr):
         n = len(arr)
-        self.subsetSum(0, 0, n, arr)   # ! Index 0  is starting index and index 1 is starting sum
+        self.subsetSum(0, 0, n, arr)   # ! Param 0  is starting index and param 1 is starting sum
 
         self.subsetSums.sort()
         return self.subsetSums
@@ -25,5 +25,5 @@ arr = [3, 1, 2]
 print(Solution().findAllSubsetSums(arr))
 
 # ? Time Complexity: O(2^N) + O(2^Nlog(2^N)) as for each of the N elements in the array, we can either choose to include / exclude it in our subset and we also have to sort the subsets sum.
-# ? Space Complexity: O(2^N) + O(log2N) as we are storing the sums of all subsets as for array of N elements, there are 2^N subsets. 
+# ? Space Complexity: O(2^N) + O(N) as we are storing the sums of all subsets as for array of N elements, there are 2^N subsets. 
 # ?                   But since we use recursion, we can consider auxillary stack space of O(N) as we can have a maximum N elements in the recursive stack space.
